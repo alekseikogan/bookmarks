7f338cf08e5b29bf22df3f500fc994ac2ea0b96e
Alexey Kogan <alekseikogan@yandex.ru>
2024-10-26 13:22:32 +0400

    сделал измение профиля и юзера

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Profile
+
 from django.contrib.auth.models import User
 
 
@@ -23,3 +25,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+   class Meta:
+      model = User
+      fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+   class Meta:
+      model = Profile
+      fields = ['date_of_birth', 'photo']
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .forms import UserRegistrationForm
+from .forms import ProfileEditForm, UserEditForm, UserRegistrationForm
+from .models import Profile
 
 
 @login_required
@@ -19,6 +20,7 @@ def register(request):
             new_user.set_password(
             user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request,
                       'account/register_done.html',
                       {'new_user': new_user})
@@ -29,22 +31,21 @@ def register(request):
                       {'user_form': user_form})
 
 
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request,
-#                                 username=cd['username'],
-#                                 password=cd['password'])
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     return HttpResponse('Успешно авторизован!')
-#                 else:
-#                     return HttpResponse('Аккаунт неактивен!')
-#             else:
-#                 return HttpResponse('Неверный логин!')
-#     else:
-#         form = LoginForm()
-#         return render(request, 'account/login.html', {'form': form})
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile)
+
+    return render(request, 'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
