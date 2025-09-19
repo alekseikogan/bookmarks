b0d2c6a49d416b8be301076e909c5c6b50f6f01a
alekseikogan <alekseikogan@yandex.ru>
2025-09-19 20:45:57 +0300

    Букмарклет работает!

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -20,3 +20,43 @@ var boxHtml = `
         <div class="images"></div>
     </div>`;
 body.innerHTML += boxHtml;
+
+function bookmarkletLaunch() {
+    bookmarklet = document.getElementById('bookmarklet');
+    var imagesFound = bookmarklet.querySelector('.images');
+    // очистить найденные изображения
+    imagesFound.innerHTML = '';
+    // показать букмарклет
+    bookmarklet.style.display = 'block';
+    // событие закрытия
+    bookmarklet.querySelector('#close')
+        .addEventListener('click', function(){
+        bookmarklet.style.display = 'none';
+    });
+// найти изображения в DOM с минимальными размерами
+    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
+    images.forEach(image => {
+        if(image.naturalWidth >= minWidth
+        && image.naturalHeight >= minHeight)
+        {
+            var imageFound = document.createElement('img');
+            imageFound.src = image.src;
+            imagesFound.append(imageFound);
+        }
+    })
+
+    // событие выбора изображения
+    imagesFound.querySelectorAll('img').forEach(image => {
+        image.addEventListener('click', function(event){
+            imageSelected = event.target;
+            bookmarklet.style.display = 'none';
+            window.open(siteUrl + 'images/create/?url='
+                + encodeURIComponent(imageSelected.src)
+                + '&title='
+                + encodeURIComponent(document.title),
+                '_blank');
+        })
+    })
+}
+// запустить букмарклет
+bookmarkletLaunch();
