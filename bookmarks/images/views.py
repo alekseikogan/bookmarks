0bed1d83dadfc37927160cb059b0dc3bb70f5798
alekseikogan <alekseikogan@yandex.ru>
2025-09-20 18:46:04 +0300

    бесконечная скролбар

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,27 +1,33 @@
-from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
-
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
 
 
 @login_required
 def image_create(request):
-    '''Создание изображение в БД.'''
-
     if request.method == 'POST':
+        # form is sent
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
+            # form data is valid
             cd = form.cleaned_data
             new_image = form.save(commit=False)
+            # assign current user to the item
             new_image.user = request.user
             new_image.save()
-            messages.success(request, 'Изображение успешно добавлено!')
+            messages.success(request, 'Image added successfully')
+            # redirect to new created image detail view
             return redirect(new_image.get_absolute_url())
     else:
+        # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
     return render(request,
                   'images/image/create.html',
@@ -30,18 +36,11 @@ def image_create(request):
 
 
 def image_detail(request, id, slug):
-    '''Детальное представление изображения'''
-
     image = get_object_or_404(Image, id=id, slug=slug)
-    context = {
-        'section': 'images',
-        'image': image,
-    }
-    return render(
-        request,
-        'images/image/detail.html',
-        context=context
-    )
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
 
 
 @login_required
@@ -60,3 +59,32 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 6)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                   {'section': 'images',
+                    'images': images})
