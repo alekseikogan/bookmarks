c3eac97f11982623e026e926ff16a14405177b04
alekseikogan <alekseikogan@yandex.ru>
2025-09-25 19:41:54 +0300

    add debug_toolbar

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'easy_thumbnails',  # 310 страница
     'sorl.thumbnail',
     'actions.apps.ActionsConfig',
+    'debug_toolbar',  # 369 страница
 
 ]
 
@@ -50,6 +51,7 @@ STORAGES = {
 }
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -177,3 +179,7 @@ SOCIAL_AUTH_PIPELINE = [
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',
                                         args=[u.username])}
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
