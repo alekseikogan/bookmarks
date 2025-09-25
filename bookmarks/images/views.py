48f4e72a6bc1a855351e029fa8edcd8b0eb52f37
alekseikogan <alekseikogan@yandex.ru>
2025-09-25 20:42:03 +0300

    add redis and logout fix

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,5 @@
+import redis
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -7,10 +9,18 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
+from django.conf import settings
 
 from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
+
+
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -40,10 +50,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 
 @login_required
